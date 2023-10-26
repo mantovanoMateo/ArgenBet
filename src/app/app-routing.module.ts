981c30a9c063c8e73caafc5cbb5112c0fec3183a
mateo <mantovanomateo@gmail.com>
2023-10-26 12:12:21 -0300

    add some routings for the new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { LogInComponent } from './components/log-in/log-in.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { MainMenuComponent } from './components/main-menu/main-menu.component';
+import { DefautlMenuComponent } from './components/defautl-menu/defautl-menu.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { MyBetsComponent } from './components/my-bets/my-bets.component';
 
 const routes: Routes = [
   {path: 'logIn', component:LogInComponent},
   {path: 'signUp', component:SignUpComponent},
-  {path: 'mainMenu', component:MainMenuComponent},
-  {path: '', redirectTo:'/mainMenu', pathMatch: 'full'}
+  {path: 'mainMenu', component:MainMenuComponent,
+   children:[
+    {path: 'defaultMenu', component:DefautlMenuComponent},
+    {path: 'teams', component:TeamsComponent},
+    {path: 'mybets', component:MyBetsComponent}
+   ]
+  },
+  {path: '', redirectTo:'/mainMenu/defaultMenu', pathMatch: 'full'}
 ];
 
 @NgModule({
