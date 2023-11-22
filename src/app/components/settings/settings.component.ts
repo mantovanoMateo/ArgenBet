35b4e22afe4ae3e3848da335cc7518eb1de48426
agustin <agustincarnessali@gmail.com>
2023-11-22 15:08:41 -0300

    finishing User story 125 with some additions to the settings menu

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
-import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { FormGroup,FormControl,Validators, ValidationErrors } from '@angular/forms';
 import { CustomValidatorsService } from 'src/app/services/custom-validators.service';
 import { User } from 'src/app/models/User';
+import { AbstractControl } from '@angular/forms';
+import { Subject, debounceTime, of, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-settings',
@@ -10,6 +12,8 @@ import { User } from 'src/app/models/User';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent {
+  passwordsMatch = false;
+  private destroy$:Subject<void> = new Subject();
   constructor(private userService: UserService, private customvalidator : CustomValidatorsService){}
 
   settingsForm = new FormGroup({
@@ -19,8 +23,39 @@ export class SettingsComponent {
     email: new FormControl(this.userService.getUserData().email, [Validators.required, Validators.email]),
     gender: new FormControl(this.userService.getUserData().gender, [Validators.required]),
     password: new FormControl(this.userService.getUserData().password, [Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]),
+    confirmPassword: new FormControl('',Validators.required),
   });
+  ngOnInit() {
+    this.settingsForm
+      .get('confirmPassword')
+      ?.setAsyncValidators(this.passwordMatchValidator.bind(this));
 
+    this.settingsForm
+      .get('password')
+      ?.valueChanges.pipe(debounceTime(300), takeUntil(this.destroy$))
+      .subscribe(() => {
+        this.settingsForm.get('confirmPassword')?.updateValueAndValidity({
+          onlySelf: true,
+          emitEvent: false,
+        });
+      });
+  }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+  private passwordMatchValidator(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> | import('rxjs').Observable<ValidationErrors | null> {
+    const password = this.settingsForm.get('password')?.value;
+    const confirmPassword = control.value;
+
+    this.passwordsMatch = password === confirmPassword;
+    return of(this.passwordsMatch ? null : { mismatch: true }).pipe(
+      debounceTime(300),
+      takeUntil(this.destroy$)
+    );
+  }
   onSubmit() {
     if (this.settingsForm.valid) {
       let user = this.userService.getUserData();
