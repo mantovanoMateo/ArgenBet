d28c79651872cdf17fcbbadf44a3140aeedbe364
agustin <agustincarnessali@gmail.com>
2023-11-22 18:23:15 -0300

    details

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -20,7 +20,6 @@ export class SettingsComponent {
     firstName: new FormControl(this.userService.getUserData().firstName, [Validators.required, Validators.maxLength(50),this.customvalidator.onlyLetters()]),
     lastName: new FormControl(this.userService.getUserData().lastName, [Validators.required, Validators.maxLength(50),this.customvalidator.onlyLetters()]),
     phone: new FormControl(this.userService.getUserData().phone, [Validators.required,Validators.pattern('[+0-9]+'),Validators.minLength(7)]),
-    email: new FormControl(this.userService.getUserData().email, [Validators.required, Validators.email, this.customvalidator.existingEmailValidator()]),
     gender: new FormControl(this.userService.getUserData().gender, [Validators.required]),
     password: new FormControl(this.userService.getUserData().password, [Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]),
     confirmPassword: new FormControl('',Validators.required),
@@ -62,7 +61,6 @@ export class SettingsComponent {
       user.firstName = this.settingsForm.get('firstName')?.value!;
       user.lastName = this.settingsForm.get('lastName')?.value!;
       user.phone = this.settingsForm.get('phone')?.value!;
-      user.email = this.settingsForm.get('email')?.value!;
       user.gender = this.settingsForm.get('gender')?.value!;
 
       user.password = this.settingsForm.get('password')?.value!; 
